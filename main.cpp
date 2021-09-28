05f113391e43a089013835214a37405cafad1731
ViKarp <91561214+ViKarp@users.noreply.github.com>
2021-09-29 00:12:41 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,28 +1,17 @@
-#include <stdio.h>
 #include <ctype.h>
 #include <string.h>
-#include <stdlib.h>
 #include <string>
-#include <cstddef>
 #include <iostream>
-#include <algorithm>
 #include <fstream>
-#include "BST.h"
-
-#define MAXWORD 100000
-struct tnode* max1 = NULL;
-struct tnode* max2 = NULL;
-struct tnode* max3 = NULL;
-
+#include "Struct.h"
 
 int main() {
     struct tnode* root;
-    char word[MAXWORD];
     root = NULL; //корень
-    while (word[0] != '0') { // условие выхода – ввод символа '0'
-        scanf("%s", word, MAXWORD); //читать по строчке
-        if (word[0] != '0') {
-            std::string st = std::string(word); //переводим в строку
+    std::ifstream inf("/home/victor21/CLionProjects/untitled/text");
+    std::string st;
+    if (inf.is_open()) {
+        while (inf >> st) {
             transform(st.begin(), st.end(), st.begin(), ::tolower); //переводим в нижний регистр
             st.erase(std::remove_if(st.begin(), st.end(), ispunct), st.end()); //удаляем знаки пунктуации
             root = addtree(root, st.data()); //создаем дерево, переводя строку обратно в char
@@ -34,6 +23,5 @@ int main() {
               << "Далее: " << max2->word << ' ' << max2->count<<std::endl
               << "На третьем месте: " << max3->word << ' ' << max3->count << std::endl;
     freemem(root); //удаляем все дерево
-    getchar();
     return 0;
 }
